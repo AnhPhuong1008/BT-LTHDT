9eea365280114071fcca317d564621c74e1576b6
AnhPhuong1008 <anhphuongnt1008@gmail.com>
2021-10-28 10:25:46 +0700

    update

--- src/com/phuongnta2008110300/lab7/TestDriveShape.java
+++ src/com/phuongnta2008110300/lab7/TestDriveShape.java
@@ -3,8 +3,14 @@ package com.phuongnta2008110300.lab7;
 public class TestDriveShape {
     public static void main(String[] args) {
         Circle circle = new Circle("hinhtron.aif");
-        Square square = new Square();
-        Triangle triangle = new Triangle();
+        circle.xoay360();
+        circle.amThanh();
+        Square square = new Square("hinhvuong.aif");
+        square.xoay360();
+        square.amThanh();
+        Triangle triangle = new Triangle("hinhtamgiac.aif");
+        triangle.xoay360();
+        triangle.amThanh();
         Amoeba amoeba = new Amoeba("amoeba.hif",5,10);
         amoeba.xoay360();
         amoeba.amThanh();
